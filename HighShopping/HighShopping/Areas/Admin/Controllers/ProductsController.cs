ebcb189603dbcf1ce02f1a12e54b718783777cea
Nguyen The Hiep <nguyenhiepskt1@gmail.com>
2022-06-08 08:14:29 +0700

    done create product, upload image product
    Signed-off-by: Nguyen The Hiep <nguyenhiepskt1@gmail.com>

--- HighShopping/HighShopping/Areas/Admin/Controllers/ProductsController.cs
+++ HighShopping/HighShopping/Areas/Admin/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using HighShopping.Infrastructure;
+using HighShopping.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,9 +16,11 @@ namespace HighShopping.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private readonly HighShoppingCartContext _context;
-        public ProductsController(HighShoppingCartContext context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductsController(HighShoppingCartContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         // GET /admin/products
@@ -30,6 +35,46 @@ namespace HighShopping.Areas.Admin.Controllers
             ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
 
             return View();
+        }
+
+        // POST /admin/products/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            ViewBag.CategoryId = new SelectList(_context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+                var slug = await _context.Products.FirstOrDefaultAsync(c => c.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Sản phẩm đã tồn tại");
+                    return View(product);
+                }
+
+                string imageName = "noimage.jpg";
+                if(product.ImageUpload != null)
+                {
+                    string uploadDir = Path.Combine(_webHostEnvironment.WebRootPath, "image/products");
+                    imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                }
+
+                product.Image = imageName;
+
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+
+                TempData["Success"] = "Thêm sản phẩm thành công!";
+
+                return RedirectToAction("Index");
+            }
+            return View(product);
         }
     }
 }
--- HighShopping/HighShopping/Migrations/20220608005637_productupload.Designer.cs
+++ HighShopping/HighShopping/Migrations/20220608005637_productupload.Designer.cs
@@ -3,14 +3,16 @@ using HighShopping.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HighShopping.Migrations
 {
     [DbContext(typeof(HighShoppingCartContext))]
-    partial class HighShoppingCartContextModelSnapshot : ModelSnapshot
+    [Migration("20220608005637_productupload")]
+    partial class productupload
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

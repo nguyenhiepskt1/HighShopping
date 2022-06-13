79233f4d80eb26ac3ecdd9ad2b645530d7c0e758
Nguyen The Hiep <nguyenhiepskt1@gmail.com>
2022-06-14 00:42:12 +0700

    done view product with category
    Signed-off-by: Nguyen The Hiep <nguyenhiepskt1@gmail.com>

---
+++ HighShopping/HighShopping/Infrastructure/CategoriesViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using HighShopping.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HighShopping.Infrastructure
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly HighShoppingCartContext _context;
+        public CategoriesViewComponent(HighShoppingCartContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await GetCategoriesAsync();
+
+            return View(categories);
+        }
+
+        private Task<List<Category>> GetCategoriesAsync()
+        {
+            return _context.Categories.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}

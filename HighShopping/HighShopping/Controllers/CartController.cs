5d3cf26f05b16d1d9597f3a9ccae83a4349a7585
Nguyen The Hiep <nguyenhiepskt1@gmail.com>
2022-06-14 23:39:42 +0700

    done cart
    Signed-off-by: Nguyen The Hiep <nguyenhiepskt1@gmail.com>

--- HighShopping/HighShopping/Controllers/CartController.cs
+++ HighShopping/HighShopping/Controllers/CartController.cs
@@ -51,7 +51,68 @@ namespace HighShopping.Controllers
 
             HttpContext.Session.SetJson("Cart", cart);
 
+            if(HttpContext.Request.Headers["X-Requested-With"] != "XMLHttpRequest")
+            {
+                return RedirectToAction("Index");
+            }
+
+            return ViewComponent("SmallCart");
+        }
+
+        // GET /cart/decrease/id
+        public IActionResult Decrease(int id)
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            CartItem cartItem = cart.Where(x => x.ProductId == id).FirstOrDefault();
+
+            if (cartItem.Quantity > 1)
+            {
+                --cartItem.Quantity;
+            }
+            else
+            {
+                cart.RemoveAll(x => x.ProductId == id);
+            }
+
+            HttpContext.Session.SetJson("Cart", cart);
+
+            if(cart.Count == 0)
+            {
+                HttpContext.Session.Remove("Cart");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+
             return RedirectToAction("Index");
+        }
+
+        // GET /cart/remove/id
+        public IActionResult Remove(int id)
+        {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart");
+
+            cart.RemoveAll(x => x.ProductId == id);
+
+            if (cart.Count == 0)
+            {
+                HttpContext.Session.Remove("Cart");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET /cart/clear
+        public IActionResult Clear()
+        {
+            HttpContext.Session.Remove("Cart");
+            return RedirectToAction("Index", "Products");
         }
     }
 }
---
+++ HighShopping/HighShopping/Models/SmallCartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HighShopping.Models
+{
+    public class SmallCartViewModel
+    {
+        public int NumberOfItems { get; set; }
+        public float TotalAmout { get; set; }
+    }
+}
